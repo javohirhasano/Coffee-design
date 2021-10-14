94bfb194f2cbf7c34f54f3eefe6ab0dd92659845
javohirhasano <ijavoxir5@gmail.com>
2021-10-15 01:08:07 +0500

    title

---
+++ main.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector(".bar1-menu")
+document.querySelector("#search3").onclick = () => {
+    navbar.classList.toggle("active")
+}
+let Search = document.querySelector(".search-bar")
+document.querySelector("#search1").onclick = () => {
+    Search.classList.toggle("active2")
+}
+let Product = document.querySelector(".bar2-menu")
+document.querySelector("#search2").onclick = () => {
+    Product.classList.toggle("active3")
+}
